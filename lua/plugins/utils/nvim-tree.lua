48dc0731ccf9c7f0cf8f336c7a3c3f397b254611
baneits <bastien.d09@laposte.net>
2026-09-02 22:36:06 +0200

    remove nvim-tree

--- lua/plugins/utils/nvim-tree.lua
+++
@@ -1,8 +0,0 @@
-vim.pack.add({
-	{ src = "https://github.com/nvim-tree/nvim-tree.lua", name = "nvim-tree" },
-})
-
-require("nvim-tree").setup({})
-vim.keymap.set("n", "<leader>e", function()
-	require("nvim-tree.api").tree.toggle()
-end, { desc = "Toggle NvimTree" })
